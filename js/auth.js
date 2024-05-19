6f790060f4027b390e0388094fd69160f5f7a036
Walter G. N. A. Murillo <45180748+walter-murillo@users.noreply.github.com>
2024-05-19 17:55:37 -0300

    Updated
    Inclusión de archivo .js para validación de datos en campos de formularios register y login

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+/**
+ * Autor/a: ANAGUA, Walter Gaston Nicolas
+ * version: 0.1
+ * fecha: 19/5/2024
+ * 
+ * Descripción: El siguiente script valida los datos ingresados o no de los campos 
+ * de formulario "registro" (register.html) e "inicio de sesión" (login.html)
+ * 
+ */
