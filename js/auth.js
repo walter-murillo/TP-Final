69b960f90a769e86acf951138a0b35e12ea8da04
Walter G. N. A. Murillo <45180748+walter-murillo@users.noreply.github.com>
2024-05-22 01:37:44 -0300

    Updated

--- js/auth.js
+++ js/auth.js
@@ -62,15 +62,15 @@ const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-solid fa-circle-check');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-solid fa-circle-xmark');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-check');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-xmark');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos[campo] = true;
 	} else {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-solid fa-circle-xmark');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-solid fa-circle-check');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-xmark');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos[campo] = false;
 	}
@@ -91,8 +91,8 @@ const validarPassword2 = () => {
 	} else {
 		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-solid fa-circle-xmark');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-solid fa-circle-check');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-circle-xmark');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-circle-check');
 		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos['password'] = true;
 	}
